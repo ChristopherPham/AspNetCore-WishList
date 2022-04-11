e1d6da5bf65c0d49e96f5bad58538b2e1d86696e
ChristopherPham <Christopher_pham@yahoo.com>
2022-04-11 01:08:18 -0500

    fix application context.

--- WishList/Data/ApplicationDBContext.cs
+++ WishList/Data/ApplicationDBContext.cs
@@ -2,10 +2,10 @@
 using WishList.Models;
 namespace WishList.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
 
-        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions options) : base(options) {  }
         public DbSet<Item> MyProperty { get; set; }
 
     }
