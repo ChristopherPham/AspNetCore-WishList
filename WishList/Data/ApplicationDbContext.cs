016b6e38331aee46ed5a13fca79a446f43961b94
ChristopherPham <Christopher_pham@yahoo.com>
2022-04-11 01:12:23 -0500

    Itmes rename

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace WishList.Data
     {
 
         public ApplicationDbContext(DbContextOptions options) : base(options) {  }
-        public DbSet<Item> MyProperty { get; set; }
+        public DbSet<Item> Items { get; set; }
 
     }
 }
