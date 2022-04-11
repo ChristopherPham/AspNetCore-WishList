88df98cad5748ca5a37f07ed2f7f8f445ae187f9
ChristopherPham <Christopher_pham@yahoo.com>
2022-04-11 00:48:49 -0500

    Add DB set items and Items class.

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+namespace WishList.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+         public ApplicationDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<Item> MyProperty { get; set; }
+
+    }
+}
